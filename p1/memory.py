fce00d8148bc6b09a970396122a2844ef05d4ff7
LucasHaug <lucas.haug@hotmail.com>
2021-05-23 19:58:15 -0300

    ✨ Add memory class

---
+++ p1/memory.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Memory():
+    def __init__(self, size) -> None:
+        self.array = np.zeros(size, dtype=np.uint8)
+
+
+    def __getitem__(self, index):
+        return self.array[index]
+
+
+    def clear(self):
+        self.array *= 0
+
+
+    def display(self):
+        np.savetxt("image.txt", self.array, fmt='%02X')
+
