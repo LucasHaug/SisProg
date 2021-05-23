e9dde8dc5bcda662d5129e32df5739206e336ef6
LucasHaug <lucas.haug@hotmail.com>
2021-05-22 21:17:19 -0300

    💩 Add test

--- p1/__main__.py
+++ p1/__main__.py
@@ -1,4 +1,24 @@
+from .loader.reading_motor import ReadingMotor
+from .loader.line_motor import LineMotor
+from .loader.events import FileEvent
+
 def main():
+    reading_motor = ReadingMotor("aaa.txt")
+    line_motor = LineMotor()
+
+    reading_motor.set_line_motor(line_motor)
+
+    reading_motor.add_event(FileEvent("open_file"))
+
+    reading_motor.run()
+
+    reading_motor.add_event(FileEvent("read"))
+    reading_motor.add_event(FileEvent("read"))
+    reading_motor.add_event(FileEvent("close_file"))
+
+    while reading_motor.run():
+        print("Running..")
+
 
 if __name__ == '__main__':
     main()
