c3af94ee64b357ad365a727cbf79bf902e0d3418
LucasHaug <lucas.haug@hotmail.com>
2021-05-23 00:27:21 -0300

    ✨ Update main

--- p1/__main__.py
+++ p1/__main__.py
@@ -1,21 +1,38 @@
+import numpy as np
+
 from .loader.reading_motor import ReadingMotor
 from .loader.line_motor import LineMotor
+from .loader.storing_motor import StoringMotor
 from .loader.events import FileEvent
 
 def main():
     reading_motor = ReadingMotor()
     line_motor = LineMotor()
+    storing_motor = StoringMotor()
 
-    reading_motor.set_file_name("aaa.txt")
+    memory = np.zeros(4096, dtype=np.uint8)
+
+    reading_motor.set_file_name("mem.txt")
 
     reading_motor.set_line_motor(line_motor)
+
     line_motor.set_reading_motor(reading_motor)
+    line_motor.set_storing_motor(storing_motor)
+
+    storing_motor.set_reading_motor(reading_motor)
+    storing_motor.set_memory_pointer(memory)
 
     reading_motor.add_event(FileEvent("open_file"))
 
-    while reading_motor.is_active():
-        reading_motor.run()
-        line_motor.run()
+    try:
+        while reading_motor.is_active():
+            reading_motor.run()
+            line_motor.run()
+            storing_motor.run()
+
+        np.savetxt("image.txt", memory, fmt='%x')
+    except KeyboardInterrupt:
+        print("[INFO] Ending Program")
 
 
 if __name__ == '__main__':
