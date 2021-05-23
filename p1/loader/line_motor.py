b2f4ed01584068f99a9aaa3412ac197613c18927
LucasHaug <lucas.haug@hotmail.com>
2021-05-23 00:25:44 -0300

    ✨ Update line motor

--- p1/loader/line_motor.py
+++ p1/loader/line_motor.py
@@ -1,5 +1,5 @@
 from ..events_motor import EventsMotor
-from .events import DataEvent, FileEvent
+from .events import FileEvent, DataEvent, LineEvent
 
 class LineMotor(EventsMotor):
     def __init__(self) -> None:
@@ -10,6 +10,9 @@ class LineMotor(EventsMotor):
 
         self.read_data = []
 
+        self.line_count = 0
+        self.data_count = 0
+
 
     def set_reading_motor(self, reading_motor) -> None:
         self.reading_motor = reading_motor
@@ -19,6 +22,13 @@ class LineMotor(EventsMotor):
         self.storing_motor = storing_motor
 
 
+    def activate(self) -> None:
+        super().activate()
+
+        self.line_count = 0
+        self.data_count = 0
+
+
     def categorize_event(self, event: DataEvent) -> str:
         if event.data == "\n":
             return "line"
@@ -26,17 +36,20 @@ class LineMotor(EventsMotor):
             return "data"
 
 
-    def _save_data(self, event) -> None:
+    def _save_data(self, event : DataEvent) -> None:
         self.read_data.append(event.data)
+        self.data_count += 1
 
         next_event = FileEvent("read")
 
         self.reading_motor.add_event(next_event)
 
 
-    def _read_line(self, event) -> None:
-        print(self.read_data)
+    def _read_line(self, event : DataEvent) -> None:
+        next_event = LineEvent(self.line_count, self.read_data, self.data_count)
 
-        next_event = FileEvent("close_file")
+        self.line_count += 1
+        self.data_count = 0
+        self.read_data = []
 
-        self.reading_motor.add_event(next_event)
+        self.storing_motor.add_event(next_event)
