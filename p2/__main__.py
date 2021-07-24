cfdfebe4c56e6742cb8e8ba1e4e40aa833dc81b4
LucasHaug <lucas.haug@hotmail.com>
2021-07-24 19:01:15 -0300

    ✨ Add base main

--- p2/__main__.py
+++ p2/__main__.py
@@ -1,7 +1,16 @@
-def main():
+from calculator.calculator import Calculator
+from calculator_gui import CalculatorGUI
 
-if __name__ == '__main__':
-    main()
+def main():
+    calculator = Calculator()
+
+    gui = CalculatorGUI(calculator.set_input, calculator.get_output)
+
+    # start the GUI
+    try:
+        gui.start()
+    except KeyboardInterrupt:
+        gui.stop()
 
 if __name__ == '__main__':
     main()
