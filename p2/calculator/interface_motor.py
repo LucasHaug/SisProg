1863988d215838584b70dfe74d8a2c441055447b
LucasHaug <lucas.haug@hotmail.com>
2021-07-26 22:56:34 -0300

    ✨ Finish interface motor

--- p2/calculator/interface_motor.py
+++ p2/calculator/interface_motor.py
@@ -1,5 +1,8 @@
 from ..events_motor import EventsMotor
-from .events import DataReadingEvent
+from .events import DataReadingEvent, ExpressionEvent
+
+from .conversion_motor import ConversionMotor
+from .evaluation_motor import EvaluationMotor
 
 from ..common import BASE_CHOICES
 
@@ -16,10 +19,13 @@ class InterfaceMotor(EventsMotor):
         self.output = ""
         self.current_base = BASE_CHOICES[2]
 
+        self.conversion_motor = ConversionMotor()
+        self.evaluation_motor = EvaluationMotor()
+
+        self.conversion_motor.set_evaluation_motor(self.evaluation_motor)
 
     def get_output(self) -> str:
         return self.output
-
 
     def categorize_event(self, event: DataReadingEvent) -> str:
         if event.data == "Cl":
@@ -31,18 +37,29 @@ class InterfaceMotor(EventsMotor):
         else:
             return "operation_element"
 
-
     def _clear_output(self, event: DataReadingEvent) -> None:
         self.output = ""
 
-
     def _calculate_result(self, event: DataReadingEvent) -> None:
-        self.output = ""
+        self.evaluation_motor.activate()
+        self.conversion_motor.activate()
 
+        self.conversion_motor.add_event(ExpressionEvent(self.output, self.current_base))
+
+        all_motors_activeted = True
+
+        while all_motors_activeted:
+            self.evaluation_motor.run()
+            self.conversion_motor.run()
+
+            all_motors_activeted = (
+                self.evaluation_motor.is_active() or self.conversion_motor.is_active()
+            )
+
+        self.output = self.conversion_motor.get_converted_output()
 
     def _change_base(self, event: DataReadingEvent) -> None:
         self.current_base = event.data
-
 
     def _store_operation_element(self, event: DataReadingEvent) -> None:
         self.output += event.data
